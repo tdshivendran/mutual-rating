901e8e1e43434af561b3073e76a9620a34950e0c
Suraj Jena <jena.suraj.k@gmail.com>
2018-10-09 00:47:26 -0600

    Mockup UI  implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Slider from 'rc-slider';
 import logo from './logo.svg';
 import './App.css';
+import 'rc-slider/assets/index.css';
+import Tooltip from 'rc-tooltip';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h2 class="right">
+            <p>8.0 <br /> <p id="text">Your<br />Rating</p></p>
+          </h2>
           <img src={logo} className="App-logo" alt="logo" />
+          <h2 class="left">
+            <p>9.0<br /> <p id="text">Community<br />Rating</p></p>
+          </h2>
           <p>
-            Edit <code>src/App.js</code> and it will hot reload.
+            How would you rate this user?
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import Tooltip from 'rc-tooltip';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+const Handle = Slider.Handle;
+
+const handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Renders to the "slider" DOM element, just below "root" as in index.HTML
+// Makes use of the rc-slider package
+
+const wrapperStyle = { width: 600, margin: 100 };
+ReactDOM.render(
+  <div>
+    <div style={wrapperStyle}>
+      <p>Accuracy</p>
+      <Slider min={0} max={10} defaultValue={5} handle={handle} />
+    </div>
+    <div style={wrapperStyle}>
+      <p>Reliability</p>
+      <Slider min={0} max={10} defaultValue={3} handle={handle} />
+    </div>
+    <div style={wrapperStyle}>
+      <p>Quality</p>
+      <Slider min={0} max={10} defaultValue={5} handle={handle} />
+    </div>
+  </div>,
+  document.getElementById('slider')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
