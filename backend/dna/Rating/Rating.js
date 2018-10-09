852caa15d0d5faab07b06036a5ba5477e4e85d71
dlee67 <dlee67@msudenver.edu>
2018-10-09 16:54:51 -0600

    Updating Rating.js and dna.json

--- backend/dna/Rating/Rating.js
+++ backend/dna/Rating/Rating.js
@@ -1,24 +1,27 @@
+function genesis () {
+  return true;
+}
+
 // Accepts an Agent's Key (which is the Ratee), and returns the ACR for that Agent.
+/// @parms{ratee}
+/// @parms{ratee, category}
+/// @parms{ratee, rater, category}
 function lookUp(ratee){
-   return ""
-}
-
-// Accepts a Rater & Ratee's Keys, and returns the AIR for the value that Rater imparted to that Ratee.
-function lookUp(ratee, rater){
-   return ""
-}
-
-// Accepts a Rater & Ratee's Keys, and returns the ICR for the value that Rater imparted to that Ratee for that respective category.
-function lookUp(ratee, rater, category){
-   return ""
+   var status = get(ratee) // Returns the Status entry, containing ACR.
+   return status.ACR
 }
 
 // Accepts a Ratee Key & a Category for whom all ratings in that category are aggregated and returned (CAR is returned).
-function inspect(ratee, category){
-   return ""
+/// @parms{ratee, category}
+function inspect(arg){
+   var ratee = API.Agent.hash
+   var rating = get(arg) // Should return an entry of containing rating according to the category.
+   return rating
 }
 
 // Accepts a Ratee's key, and a Category, and commits that rating entry to the Rater's source chain.
-function publish(ratee, category){
-   return ""
+/// @parms{content,rating,timestamp}
+function publish(arg){
+   var publish_schema = {arg.content, arg.rating, arg.timestamp}
+   commit("publish", publish_schema)
 }
