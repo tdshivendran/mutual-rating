1f7965010aaf706150bbab467d35883792f30abf
Suraj Jena <jena.suraj.k@gmail.com>
2018-10-09 20:14:09 -0600

    Minor edits

--- backend/dna/Rating/Rating.js
+++ backend/dna/Rating/Rating.js
@@ -5,7 +5,7 @@
  ************************************************************
  * Individual Categorical Rating [ICR] (published by Rater)
  * Aggregate Individual Rating [AIR] (each Rater's rating
- *    aggregated over a selected Ratee)
+ *    aggregated over the categories of a selected Ratee)
  * Categorical Aggregate Rating [CAR] (each Ratee's ratings
  *    aggregated over a particular category)
  * Aggregate Community Rating [ACR] (every Rater's rating
@@ -22,7 +22,7 @@ function genesis()
 
 /* Accepts an Agent's Key (which is the Ratee), and returns the
  * ACR for that Agent.
- * @params{ratee}
+ * @param arg is String(ratee)
  */
 function computeACR(arg)
 {
@@ -32,7 +32,7 @@ function computeACR(arg)
 
 /* Accepts a Rater & Ratee's Keys, and returns the AIR for the value
  * that Rater imparted to that Ratee.
- * @params{ratee, category}
+ * @param arg is JSON{ratee, category}
  */
 function computeAIR(arg)
 {
@@ -42,7 +42,7 @@ function computeAIR(arg)
 
 /* Accepts a Rater & Ratee's Keys, and returns the ICR for
  * the value that Rater imparted to that Ratee for that respective category.
- * @params{ratee, rater, category}
+ * @param arg is JSON{ratee, rater, category}
  */
 function computeICR(arg)
 {
@@ -52,7 +52,7 @@ function computeICR(arg)
 
 /* Accepts a Ratee Key & a Category for whom all ratings in
  * that category are aggregated and returned (CAR is returned).
- * @params{ratee, category}
+ * @param arg is JSON{ratee, category}
  */
 function computeCAR(arg)
 {
@@ -62,7 +62,7 @@ function computeCAR(arg)
 
 /* Awaits a notification from any Rater in the DHT
  * a trigger for publishStatus()
- * @params{ratee, category}
+ * @param arg is JSON{ratee, category}
  */
 function awaitRating(arg)
 {
@@ -72,18 +72,18 @@ function awaitRating(arg)
 
 /* Accepts a Ratee's key, and a Category, and commits that rating
  * entry to the Rater's source chain.
- * @parms{Ratee, Category}
+ * @param arg is JSON{Ratee, Category}
  */
 function publishRating(arg)
 {
-    
+
     return;
 }
 
 
 /* Accepts a Ratee's key, and a Category, and commits that rating
  * entry to the Rater's source chain.
- * @parms{entryHash}
+ * @param arg is String(entryHash)
  */
 function publishStatus(arg)
 {
