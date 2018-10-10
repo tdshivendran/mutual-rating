fa7ead2f1ef6d96c5bad65538636276e085f4376
Suraj Jena <jena.suraj.k@gmail.com>
2018-10-09 18:09:10 -0600

    Rating.js minor edit

--- backend/dna/Rating/Rating.js
+++ backend/dna/Rating/Rating.js
@@ -1,24 +1,29 @@
-// Accepts an Agent's Key (which is the Ratee), and returns the ACR for that Agent.
+// Accepts an Agent's Key (which is the Ratee), and returns the
+// ACR for that Agent.
 function lookUp(ratee){
    return ""
 }
 
-// Accepts a Rater & Ratee's Keys, and returns the AIR for the value that Rater imparted to that Ratee.
+// Accepts a Rater & Ratee's Keys, and returns the AIR for the value that
+// Rater imparted to that Ratee.
 function lookUp(ratee, rater){
    return ""
 }
 
-// Accepts a Rater & Ratee's Keys, and returns the ICR for the value that Rater imparted to that Ratee for that respective category.
+// Accepts a Rater & Ratee's Keys, and returns the ICR for the value that
+// Rater imparted to that Ratee for that respective category.
 function lookUp(ratee, rater, category){
    return ""
 }
 
-// Accepts a Ratee Key & a Category for whom all ratings in that category are aggregated and returned (CAR is returned).
+// Accepts a Ratee Key & a Category for whom all ratings in that
+// category are aggregated and returned (CAR is returned).
 function inspect(ratee, category){
    return ""
 }
 
-// Accepts a Ratee's key, and a Category, and commits that rating entry to the Rater's source chain.
+// Accepts a Ratee's key, and a Category, and commits that rating
+// entry to the Rater's source chain.
 function publish(ratee, category){
    return ""
 }
